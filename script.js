055848ad2fd67f9866ced8c9c8e3a487bc5449d1
Zheka Baila Arkan <115915464+ZhekaBailaArkan@users.noreply.github.com>
2022-11-20 10:06:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,204 @@
+function move() {
+    var move = document.getElementById('judul');
+    var show = document.getElementById('descripsi');
+    var blur = document.getElementById('blur');
+
+    // move.style.top = '3%';
+    move.style.left = '2%';
+    show.style.opacity = '1';
+    show.style.animation = 'openingLeft 1s'
+    blur.style.backgroundColor = 'rgba(0, 0, 0, 0.681)';
+}
+
+
+function zoomOut1() {
+    var zoom = document.getElementById('image-1');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn1() {
+    var zoomIn = document.getElementById('image-1');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut2() {
+    var zoom = document.getElementById('image-2');
+    zoom.style.width = '490px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '350px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn2() {
+    var zoomIn = document.getElementById('image-2');
+    zoomIn.style.width = '250px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut3() {
+    var zoom3 = document.getElementById('image-3');
+    zoom3.style.width = '320px';
+    zoom3.style.height = '300px';
+    zoom3.style.position = 'Absolute';
+    zoom3.style.right = '450px'
+    zoom3.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn3() {
+    var zoomIn3 = document.getElementById('image-3');
+    zoomIn3.style.width = '150px';
+    zoomIn3.style.height = '150px';
+    zoomIn3.style.right = '30px';
+    zoomIn3.style.boxShadow = 'none'
+}
+
+function zoomOut4() {
+    var zoom = document.getElementById('image-4');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn4() {
+    var zoomIn = document.getElementById('image-4');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut5() {
+    var zoom = document.getElementById('image-5');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn5() {
+    var zoomIn = document.getElementById('image-5');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut6() {
+    var zoom = document.getElementById('image-7');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn6() {
+    var zoomIn = document.getElementById('image-7');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut7() {
+    var zoom = document.getElementById('image-8');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn7() {
+    var zoomIn = document.getElementById('image-8');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+function zoomOut8() {
+    var zoom = document.getElementById('image-9');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn8() {
+    var zoomIn = document.getElementById('image-9');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+
+function zoomOut9() {
+    var zoom = document.getElementById('image-10');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn9() {
+    var zoomIn = document.getElementById('image-10');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+
+
+function zoomOut10() {
+    var zoom = document.getElementById('image-11');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn10() {
+    var zoomIn = document.getElementById('image-11');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
+
+
+
+function zoomOut11() {
+    var zoom = document.getElementById('image-13');
+    zoom.style.width = '320px';
+    zoom.style.height = '300px';
+    zoom.style.position = 'Absolute';
+    zoom.style.right = '450px'
+    zoom.style.boxShadow = '0px 0px 30px 0.5px black'
+}
+
+function zoomIn11() {
+    var zoomIn = document.getElementById('image-13');
+    zoomIn.style.width = '150px';
+    zoomIn.style.height = '150px';
+    zoomIn.style.right = '30px';
+    zoomIn.style.boxShadow = 'none'
+}
